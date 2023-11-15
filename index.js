7dd298517bcfe9daf682aa03ff7b3da978f1b2ed
abiyyu03 <abiyyu652@gmail.com>
2023-11-15 11:20:57 +0700

    update : add dotenv configuration file

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const path = require('path');
 const { Pool, Client } = require('pg');
+require('dotenv').config();
 
 const init = async () => {
 	//database configuration
 	const client = new Client({
-		user: 'postgres',
-		host: '127.0.0.1',
-		database: 'postgres',
-		password: 'abiyyucakra99',
-		port: 5432,
+		user: process.env.DB_USER,
+		host: process.env.DB_HOST,
+		database: process.env.DB_DATABASE,
+		password: process.env.DB_PASSWORD,
+		port: process.env.DB_PORT,
 	});
 
 	//connect to the database
@@ -20,8 +21,8 @@ const init = async () => {
 	});
 
 	const server = Hapi.server({
-		port: 3000,
-		host: 'localhost',
+		port: process.env.APP_PORT,
+		host: process.env.APP_HOST,
 		routes: {
 			cors: {
 				origin: ['*'],
