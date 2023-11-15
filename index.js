61e1e2ffdcdb1bab848137df3ddceb810b637e7c
abiyyu03 <abiyyu652@gmail.com>
2023-11-15 10:12:44 +0700

    update : connecting to postgres server

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const path = require('path');
+const { Pool, Client } = require('pg');
 
 const init = async () => {
+	//database configuration
+	const client = new Client({
+		user: 'postgres',
+		host: '127.0.0.1',
+		database: 'postgres',
+		password: 'abiyyucakra99',
+		port: 5432,
+	});
+
+	//connect to the database
+	client.connect(function (err, res) {
+		if (err) throw err;
+		console.log('Postgres connection succeeded');
+	});
+
 	const server = Hapi.server({
 		port: 3000,
 		host: 'localhost',
