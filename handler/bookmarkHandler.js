d039e408d11b10080436994bf3989cc74740c1cc
abiyyu03 <abiyyu652@gmail.com>
2023-11-27 14:32:55 +0700

    implement error handling in bookmarkHandler

--- handler/bookmarkHandler.js
+++ handler/bookmarkHandler.js
@@ -5,89 +5,113 @@
 const client = require('../config/database');
 
 const getAll = async (request, h) => {
-	//query
-	const all = await client.query(`SELECT * FROM bookmarks`);
-	return h
-		.response({
-			status: 'Success',
-			message: 'Data fetched successfully',
-			total: all.rows.length,
-			data: all.rows,
-		})
-		.code(200);
+	try {
+		const all = await client.query(`SELECT * FROM bookmarks`);
+		return h
+			.response({
+				status: 'Success',
+				message: 'Data fetched successfully',
+				total: all.rows.length,
+				data: all.rows,
+			})
+			.code(200);
+	} catch (error) {
+		return h.response(errorResponse(error.message));
+	}
 };
 
 const createData = async (request, h) => {
-	const { postId, userId } = request.payload;
+	try {
+		const { postId, userId } = request.payload;
 
-	const createdAt = new Date();
-	console.log(createdAt);
-	const updatedAt = createdAt;
+		const createdAt = new Date();
+		const updatedAt = createdAt;
 
-	//create data
-	const create = await client.query(
-		`INSERT INTO bookmarks (post_id, user_id, created_at, updated_at) VALUES($1, $2, $3, $4) RETURNING *`,
-		[postId, userId, createdAt, updatedAt],
-	);
+		//create data
+		const create = await client.query(
+			`INSERT INTO bookmarks (post_id, user_id, created_at, updated_at) VALUES($1, $2, $3, $4) RETURNING *`,
+			[postId, userId, createdAt, updatedAt],
+		);
 
-	return h
-		.response({
-			status: 'Success',
-			message: 'Data created successfully !',
-			data: create.rows[0],
-		})
-		.code(201);
+		return h
+			.response({
+				status: 'Success',
+				message: 'Data created successfully !',
+				data: create.rows[0],
+			})
+			.code(201);
+	} catch (error) {
+		return h.response(errorResponse(error.message));
+	}
 };
 
 const updateData = async (request, h) => {
-	const { postId, userId } = request.payload;
-	const { id } = request.params;
+	try {
+		const { postId, userId } = request.payload;
+		const { id } = request.params;
 
-	const updatedAt = new Date().toISOString;
+		const updatedAt = new Date().toISOString;
 
-	//update data
-	const update = await client.query(
-		`UPDATE bookmarks SET post_id=$1, user_id=$2, updated_at=$3 WHERE id = $4 RETURNING *`,
-		[postId, userId, updatedAt, id],
-	);
+		//update data
+		const update = await client.query(
+			`UPDATE bookmarks SET post_id=$1, user_id=$2, updated_at=$3 WHERE id = $4 RETURNING *`,
+			[postId, userId, updatedAt, id],
+		);
 
-	return h
-		.response({
-			status: 'Success',
-			message: 'Data updated successfully !',
-			data: update.rows[0],
-		})
-		.code(200);
+		if (update.rowCount <= 0) throw new Error(`Data dengan id ${id} tidak tersedia`);
+
+		return h
+			.response({
+				status: 'Success',
+				message: 'Data updated successfully !',
+				data: update.rows[0],
+			})
+			.code(200);
+	} catch (error) {
+		return h.response(errorResponse(error.message));
+	}
 };
 
 const deleteData = async (request, h) => {
-	const { id } = request.params;
+	try {
+		const { id } = request.params;
 
-	//delete data
-	const deleted = await client.query(`DELETE FROM bookmarks WHERE id = $1 RETURNING *`, [id]);
+		//delete data
+		const deleted = await client.query(`DELETE FROM bookmarks WHERE id = $1 RETURNING *`, [id]);
 
-	return h
-		.response({
-			status: 'Success',
-			message: 'Data deleted successfully !',
-			data: deleted.rows[0],
-		})
-		.code(200);
+		if (deleted.rowCount <= 0) throw new Error(`Data dengan id ${id} tidak tersedia`);
+
+		return h
+			.response({
+				status: 'Success',
+				message: 'Data deleted successfully !',
+				data: deleted.rows[0],
+			})
+			.code(200);
+	} catch (error) {
+		return h.response(errorResponse(error.message));
+	}
 };
 
 const getById = async (request, h) => {
-	const { id } = request.params;
+	try {
+		const { id } = request.params;
 
-	//delete data
-	const dataById = await client.query(`SELECT * FROM bookmarks WHERE id = $1`, [id]);
+		//delete data
+		const dataById = await client.query(`SELECT * FROM bookmarks WHERE id = $1`, [id]);
 
-	return h
-		.response({
-			status: 'Success',
-			message: `Data fetched successfully`,
-			data: dataById.rows[0],
-		})
-		.code(200);
+		if (dataById.rowCount <= 0) throw new Error(`Data dengan id ${id} tidak tersedia`);
+
+		return h
+			.response({
+				status: 'Success',
+				message: `Data fetched successfully`,
+				data: dataById.rows[0],
+			})
+			.code(200);
+	} catch (error) {
+		return h.response(errorResponse(error.message));
+	}
 };
 
 module.exports = { getAll, createData, updateData, deleteData, getById };
